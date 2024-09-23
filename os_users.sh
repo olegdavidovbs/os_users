68950a7b6e9be77c91cc9a42df77a4ff96551007
Constantine Priemski <139596869+constantine-priemski@users.noreply.github.com>
2024-09-23 06:49:40 -0400

    init

---
+++ os_users.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "OS Version:"
+lsb_release -a 2>/dev/null || cat /etc/os-release
+echo "------------------------------------"
+
+echo "Users with Bash shell:"
+cat /etc/passwd | grep "/bin/bash" | cut -d: -f1
+echo "------------------------------------"
+
+echo "Open Ports:"
+sudo lsof -i -P -n | grep LISTEN
+echo "------------------------------------"
